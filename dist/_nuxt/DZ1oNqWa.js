a5394c1746a98d8af49b5274532fbe7dc4227931
john-muinde <jhnmuinde@gmail.com>
2024-12-10 09:55:35 +0300

    removing output and using dist

---
+++ dist/_nuxt/DZ1oNqWa.js
@@ -0,0 +1,34 @@
+const e=`**WARRANTY TERMS**
+
+1. **Equipment supplied by us will carry Manufacturer’s Warranty for 1 year from the Date of Delivery.**
+   1. All gas installation will be under warranty for 1 year.
+   2. All refrigeration installations will be under warranty for 6 months.
+   3. Electrical fittings do not carry any warranty because we don’t have the control over quality of the power supply. Goods quoted are of high quality and can last for very long if power guards are fitted to take care of voltage spikes and phase failure are installed.
+2. For optimum working of machines, it is essential that the supply of other services are of the required specifications and to be provided by the client:
+   1. Water should be soft and clear.
+   2. The drain should be the right size and right position.
+   3. Gas should be of the right size and right pressure.
+   4. The electrical supply should be stabilized and safety from phase failure, single phasing, and short circuits.
+   5. Trained and qualified personnel authorized to clean, and maintain equipment.
+3. Equipment supplied will be of the right specification as per the quotation and warranty terms are only applicable if Sheffield technicians have done the installation.
+4. Warranty is NOT applicable if the client selects different equipment of make, model, or capacity than what is recommended that may strain the efficiency & effectiveness of the equipment.
+5. Warranty is only valid if our technicians execute the installation. Installation done independently by the client will render the warranty null and void.
+6. Warranty is only valid if our technicians execute the service/ repair works. Any service, repair work, and replacement of parts done by the client will render the warranty null and void.
+7. Warranty period applies from the date of delivery.
+8. Warranty does not cover the cost of transport, food, or accommodation. It only covers the cost of the replacement of spare parts without any charge.
+9. Warranty terms are not applicable for damages caused by negligent use, unauthorized repairs carried out by others, wear and tear, faulty supply of gas, water, or power, etc.
+10. Only our technicians should be authorized to carry out service and repairs to validate the warranty.
+11. Warranty terms apply only if the full cost of equipment has been paid to us. We cannot undertake repair and maintenance if the equipment has not been paid for in full.
+12. We shall either replace the malfunctioning spare parts or refund the cost of spare parts. The client cannot claim other consequential damages like loss of business, profits, etc.
+13. Repair and maintenance under guarantee will be carried out during work hours i.e. between 8.00 am and 6.00 pm, from Monday to Saturday only.
+14. Warranty cost out of Nairobi – Travel, Food, and Accommodation of Engineer/Technician will be in the scope of the client.
+15. Warranty claims will not be accepted without warranty certificate invoice reference.
+16. Replacement of the spares does not extend or restart the warranty.
+17. All costs of re-installation & relocation, cartage, freight, and insurance associated with this warranty will be paid by the claimant.
+18. Sheffield provides professional training and advice regarding the usage of goods supplied, this should be respected to validate the warranty.
+19. Warranty does not cover glass breakage and other physical elements of the equipment. The warranty only covers spares.
+
+**LEGAL GOVERNANCE**
+
+These terms and conditions are governed under the jurisdiction of the Republic of Kenya.
+`;export{e as default};
